62273d563c8df09d836f643f98436c749e66af62
Joshua cotton <62710104+death12092@users.noreply.github.com>
2025-02-18 09:16:51 +0000

    level manager

---
+++ Assets/levelmanager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelmanager : MonoBehaviour
+{
+    public levelmanager main;
+
+    public Transform start;
+
+    private void Awake()
+    {
+        main = this
+    }
+}
